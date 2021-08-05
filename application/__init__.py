78dc6571bc8a5f2d485f1bee7a82fd2fa02b7e29
diptoghosh <51697225+diptoghosh@users.noreply.github.com>
2021-08-05 20:32:18 +0530

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+from flask_mqtt import Mqtt
+import eventlet
 
 db = SQLAlchemy()
 
+eventlet.monkey_patch()
+
 app = Flask(__name__, instance_relative_config=False)
 app.config.from_object('config.Config')
+
+# MQTT
+mqtt = Mqtt(app)
 socketio = SocketIO(app)
+
 db.init_app(app)
 
 from . import routes, models
