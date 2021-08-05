2da8e4a23754871375f5b703cfec9145f9f9cfda
diptoghosh <51697225+diptoghosh@users.noreply.github.com>
2021-08-05 20:29:19 +0530

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
 
 db = SQLAlchemy()
 
 app = Flask(__name__, instance_relative_config=False)
 app.config.from_object('config.Config')
+socketio = SocketIO(app)
 db.init_app(app)
 
 from . import routes, models
