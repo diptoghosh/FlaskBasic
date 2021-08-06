ef38bde5c62046496ba70767fcb74bf0a2985896
diptoghosh <51697225+diptoghosh@users.noreply.github.com>
2021-08-06 18:02:07 +0530

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,16 @@
 from flask import request, render_template, redirect, send_file
-from datetime import datetime
+from datetime import datetime, timedelta
+import requests
 from application import app, db, mqtt, socketio
 from .models import ContactMe
+
+def weather_report(location):
+    api_key = "3ac8fa7abbf49bab6cce3ae2127e033a"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    complete_url = base_url + "appid=" + api_key + "&q=" + location
+    response = requests.get(complete_url)
+    response_json = response.json()
+    return response_json
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -23,7 +32,24 @@ def tools():
 
 @app.route('/links', methods=['GET','POST'])
 def links():
-    return render_template('links.html')
+    selected = ''
+    weather_main = ''
+    weather_temp = ''
+    weather_humid = ''
+    weather_sunrise = ''
+    weather_sunset = ''
+    if request.method == 'POST':
+        report_dict = weather_report(request.form['_location'])
+        weather_main = 'Weather:     ' + report_dict["weather"][0]["description"].title()
+        weather_temp = 'Temperature: ' + str(round(report_dict["main"]["temp"] - 273, 1)) + ' degC'
+        weather_humid ='Humidity:    ' + str(round(report_dict["main"]["humidity"], 1)) + ' %'
+        sunrise_dt = datetime.utcfromtimestamp(report_dict['sys']['sunrise']) + timedelta(hours=5,minutes=30)
+        sunset_dt = datetime.utcfromtimestamp(report_dict['sys']['sunrise']) + timedelta(hours=5,minutes=30)
+        weather_sunrise = 'Sunrise:     ' + sunrise_dt.strftime('%Y-%m-%d %H:%M:%S')
+        weather_sunset = 'Sunset:      ' + sunset_dt.strftime('%Y-%m-%d %H:%M:%S')
+        selected = request.form['_location']
+    return render_template('links.html', selected= selected, weather_main=weather_main, weather_temp=weather_temp, weather_humid=weather_humid,\
+        weather_sunrise=weather_sunrise, weather_sunset=weather_sunset)
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
